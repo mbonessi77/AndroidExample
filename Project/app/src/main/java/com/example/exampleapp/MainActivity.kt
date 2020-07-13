6f453ce6e71d523882611b34281d420b843ed383
Michael Bonessi <michael.bonessi@coxautoinc.com>
2020-07-13 14:59:11 -0500

    Add base list fragment

--- Project/app/src/main/java/com/example/exampleapp/MainActivity.kt
+++ Project/app/src/main/java/com/example/exampleapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.exampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = ListFragment()
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fl_container, fragment).commit()
     }
 }
