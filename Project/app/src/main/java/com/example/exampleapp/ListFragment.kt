5842beaa41002977d832031109f7fd080dfb4b4a
Michael Bonessi <michael.bonessi@coxautoinc.com>
2020-07-14 14:36:29 -0500

    observe live data

--- Project/app/src/main/java/com/example/exampleapp/ListFragment.kt
+++ Project/app/src/main/java/com/example/exampleapp/ListFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.exampleapp.model.KanjiObject
 import com.example.exampleapp.recyclerview.KanjiAdapter
 import com.example.exampleapp.viewmodel.KanjiListViewModel
 
@@ -17,6 +19,12 @@ class ListFragment : Fragment() {
     lateinit var adapter: KanjiAdapter
     lateinit var viewModel: KanjiListViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(KanjiListViewModel::class.java)
+        observeViewModel()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,18 +32,20 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_list, container, false)
         recyclerView = v.findViewById(R.id.rv_list)
-        viewModel = ViewModelProviders.of(this).get(KanjiListViewModel::class.java)
-        adapter = KanjiAdapter()
-        recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context)
 
-        val list = viewModel.getList()
-        list?.let {
-            adapter.setDataSet(it)
-        } ?: kotlin.run {
-            Toast.makeText(context, "Call failed", Toast.LENGTH_SHORT).show()
-        }
+        viewModel.getList()
 
         return v
+    }
+
+    private fun observeViewModel() {
+        val observer = Observer<List<KanjiObject>> {
+            adapter = KanjiAdapter()
+            adapter.setDataSet(it)
+            recyclerView.adapter = adapter
+        }
+
+        viewModel.kanjiList.observeForever(observer)
     }
 }
--- Project/app/src/main/java/com/example/exampleapp/viewmodel/KanjiListViewModel.kt
+++ Project/app/src/main/java/com/example/exampleapp/viewmodel/KanjiListViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.exampleapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.exampleapp.model.KanjiObject
 import com.example.exampleapp.network.KanjiInterface
@@ -9,11 +10,12 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class KanjiListViewModel : ViewModel() {
-    private var kanjiList: List<KanjiObject>? = ArrayList()
+    val kanjiList = MutableLiveData<List<KanjiObject>>()
 
-    fun getList(): List<KanjiObject>? {
+    fun getList() {
         val service = RetrofitLayer().getInstance().create(KanjiInterface::class.java)
         val call = service.getAllKanji()
+
         call.enqueue(object : Callback<List<KanjiObject>> {
             override fun onFailure(call: Call<List<KanjiObject>>, t: Throwable) {
                 print("Call failed")
@@ -21,11 +23,9 @@ class KanjiListViewModel : ViewModel() {
 
             override fun onResponse(call: Call<List<KanjiObject>>, response: Response<List<KanjiObject>>) {
                 response.body()?.let {
-                    kanjiList = response.body()
+                    kanjiList.postValue(response.body())
                 }
             }
         })
-
-        return kanjiList
     }
 }
